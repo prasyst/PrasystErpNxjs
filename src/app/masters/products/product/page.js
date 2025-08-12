b393fa196f75672f08bb7183bcd20b5ada1f28f1
Prasyst <prasyst@gmail.com>
2025-08-12 11:14:29 +0530

    test

---
+++ src/app/masters/products/product/page.js
@@ -0,0 +1,6 @@
+// app/order/page.js
+import ProductMstComponent from '@/components/masters/products/product/ProductMstComponent';
+
+export default function ProductPage() {
+  return <ProductMstComponent />;
+}
