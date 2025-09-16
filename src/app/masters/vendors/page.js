31228476a82ea2a4e0f77a3626ba6d35438d9b80
Prasyst <prasyst@gmail.com>
2025-09-16 12:27:13 +0530

    Changes

--- src/app/masters/vendors/page.js
+++ src/app/masters/vendors/page.js
@@ -1,5 +1,12 @@
+// app/order/page.js
+
 import CreditorsSuppliersMst from "@/components/masters/Vendors/creditorsSuppliers/creditorsSuppliersMst";
+import { Suspense } from 'react';
 
 export default function CreditorsSuppliersPage() {
-  return <CreditorsSuppliersMst />;
+  return (
+    <Suspense>
+      <CreditorsSuppliersMst />
+    </Suspense>
+  );
 }
