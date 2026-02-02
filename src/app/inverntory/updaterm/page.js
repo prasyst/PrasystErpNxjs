22db2d68452925bf7ca01b5f7c183708ab01ddd9
prasyst <prasyst@gmail.com>
2026-02-02 17:19:45 +0530

    update routing rm ,trims

---
+++ src/app/inverntory/updaterm/page.js
@@ -0,0 +1,11 @@
+
+import { Suspense } from 'react';
+import UpdateRm from '../../../components/inventory/TnaComp/UpdateRm';
+
+export default function updaterouting() {
+return (
+    <Suspense>
+      <UpdateRm/>
+    </Suspense>
+  );
+}
---
+++ src/app/inverntory/updatetrims/page.js
@@ -0,0 +1,10 @@
+
+import { Suspense } from 'react';
+import UpdateTrims from '../../../components/inventory/TnaComp/UpdateTrims';
+export default function updatetrims() {
+return (
+    <Suspense>
+      <UpdateTrims />
+    </Suspense>
+  );
+}
